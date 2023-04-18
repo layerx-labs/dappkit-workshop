e36a434751e526955e90c83ce947e7027afb8aa8
Helder Vasconcelos <heldervasc@bearstouch.com>
2023-04-18 10:56:32 +0100

    Exercise 1 Working

--- scripts/exercise1.ts
+++ scripts/exercise1.ts
@@ -1,31 +1,42 @@
 import { ethers } from "hardhat";
 import { ERC20, Web3Connection } from "@taikai/dappkit"
-import Config  from "../hardhat.config"
 import Keys from "../data/accounts-testing"
 
+/**
+ * Exercise 1: Solution
+ */
 async function main() { 
-    const rpcUrl = "http://localhost:8545";
+    
+    const tokenName = "GM Dappkit Token";
+    const tokenSymbol =  "GMD";
+    // the total amount of the token (with 18 decimals; 100M = 1000000000000000000000000)
+    const totalSupply  = "10000000".concat("000000000000000000");      
     const [owner, otherAccount] = Keys;
+    
+    // Web3 Connection Configuration Settings    
     const web3Con = new Web3Connection({
       debug: true,
-      web3Host: rpcUrl,
+      web3Host: "http://localhost:8545",
       privateKey: owner.privKey,       
     });
-    // the total amount of the token (with 18 decimals; 100M = 1000000000000000000000000)
-    const totalSupply  = "10000000".concat("000000000000000000");
     // 1. Deploy GM Dappkit Token 
     const erc20Deployer = new ERC20(web3Con);    
     const txReceipt = await erc20Deployer.deployJsonAbi(
-      "GM Dappkit Token", // the name of the token
-      'DKT', // the symbol of the token
+      tokenName, 
+      tokenSymbol, 
       totalSupply, 
       owner.address,
     );
     console.log("Dappkit WorkShop Token to ", txReceipt.contractAddress);
-    // 2. Transfer X to another account
+    // 2. Load the ERC-20 Modal with the deployed contract
     const gmDappkit = new ERC20(web3Con, txReceipt.contractAddress);    
+    
+    // 3. Transfer X to another account
     await gmDappkit.transfer(otherAccount.address, 1000);
-    console.log("My Balance is ", await gmDappkit.balanceOf(otherAccount.address));
+
+    // 4. Read the balance 
+    const balance = await gmDappkit.balanceOf(otherAccount.address);
+    console.log(`Other Account Balance is ${balance} ${tokenSymbol}`);
 }
 
 
